72c4863b0d8c832035c020aa388577f611933bc3
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-08 23:15:39 +0500

    Error boundaries

--- app/root.jsx
+++ app/root.jsx
@@ -5,7 +5,26 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <html>
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {/* add the UI you want your users to see */}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return (
