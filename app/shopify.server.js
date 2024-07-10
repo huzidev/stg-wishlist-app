ff77fe4e18ea1a0c85906b9130a550a44904835d
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-10 19:38:00 +0500

    Default session settings

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -8,7 +8,7 @@ import {
   shopifyApp
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-// import prisma from "./db.server";
+import prisma from "./db.server";
 // import Shopify from "@shopify/shopify-api";
 export const STANDARD_PLAN = 'Standard';
 export const ADVANCED_PLAN = 'Advanced';
@@ -20,7 +20,8 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(process.env.POSTGRES_PRISMA_URL),
+  sessionStorage: new PrismaSessionStorage(prisma),
+  // sessionStorage: new PrismaSessionStorage(process.env.POSTGRES_PRISMA_URL),
   // sessionStorage: new Shopify.Session.PostgreSQLSessionStorage(new URL(process.env.POSTGRES_PRISMA_URL)),
   distribution: AppDistribution.AppStore,
   restResources,
