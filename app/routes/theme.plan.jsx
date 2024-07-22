5d592d2101a7129180c7ad5c38c7df10ac0724b6
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-23 03:01:58 +0500

    vercel test

--- app/routes/theme.plan.jsx
+++ app/routes/theme.plan.jsx
@@ -2,6 +2,7 @@ import { json } from "@remix-run/node";
 import { globalHeaders } from "~/utils/data";
 
 export async function loader() {
+  console.log("SW is this running?? VERCEL");
   const store = await prisma.store.findFirst({
     where: {
       shopify_url: "stg-wishlist-store.myshopify.com",
