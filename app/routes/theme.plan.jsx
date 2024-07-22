79920c388ad4d44b2f2e91e757775dc0a0c8790c
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-07-23 02:52:13 +0500

    theme-url updated

--- app/routes/theme.plan.jsx
+++ app/routes/theme.plan.jsx
@@ -2,10 +2,9 @@ import { json } from "@remix-run/node";
 import { globalHeaders } from "~/utils/data";
 
 export async function loader() {
-  console.log("SW what is this.shopUrl", this.shopUrl);
   const store = await prisma.store.findFirst({
     where: {
-      shopify_url: this.shopUrl,
+      shopify_url: "stg-wishlist-store.myshopify.com",
     },
   });
   return json(
